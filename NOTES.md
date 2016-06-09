20903a9651092f12c99a3cae17fc9081688a5e6a
petecarapetyan <pete@couldbe.net>
2016-06-08 21:09:45 -0500

    project complete first draft

--- NOTES.md
+++ NOTES.md
@@ -1,3 +1,9 @@
 ### SplitIterable NOTES:
 
- * no notes yet
+Run XahTest. The route itself is one line. Bafflingly simple.
+
+### Funny Story
+
+The first time I had to split a list of objects, I looked for a long time in all 167 examples of .split( that I could find in camel-core. I was looking for an argument to use with split( that would tell it what to expect. But when I finally found it I wouldn't believe it and just kept looking.
+
+When a body is an iterable, all you need is split().body(). Too simple for me to believe. 
--- src/test/java/jammazwan/xah/XahTest.java
+++ src/test/java/jammazwan/xah/XahTest.java
@@ -1,21 +1,38 @@
 package jammazwan.xah;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.camel.component.mock.MockEndpoint;
 import org.apache.camel.test.spring.CamelSpringTestSupport;
 import org.junit.Test;
 import org.springframework.context.support.AbstractXmlApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import jammazwan.entity.Employee;
+
 public class XahTest extends CamelSpringTestSupport {
 
-    @Override
-    protected AbstractXmlApplicationContext createApplicationContext() {
-        return new ClassPathXmlApplicationContext("META-INF/spring/camel-context.xml");
-    }
+	@Override
+	protected AbstractXmlApplicationContext createApplicationContext() {
+		return new ClassPathXmlApplicationContext("META-INF/spring/camel-context.xml");
+	}
 
-    @Test
-    public void testXah() throws Exception {
-        String reply = template.requestBody("direct:xah", "No Meaning Here", String.class);
-		assertEquals("My No Meaning Here", reply);
-    }
+	@Test
+	public void testXah() throws Exception {
+		MockEndpoint mock = getMockEndpoint("mock:assert");
+		mock.expectedMessageCount(6);
+
+		List<Employee> employees = new ArrayList<Employee>();
+		employees.add(new Employee("Honolulu", "Ajax Coders", "Jan", "Fersing"));
+		employees.add(new Employee("Honolulu", "Ajax Coders", "Mort", "Miller"));
+		employees.add(new Employee("Honolulu", "Ajax Coders", "Mary", "Smith"));
+		employees.add(new Employee("Honolulu", "Ajax Coders", "Jacob", "Applebaum"));
+		employees.add(new Employee("Honolulu", "Ajax Coders", "Tommy", "Dokortnick"));
+		employees.add(new Employee("Honolulu", "Ajax Coders", "Barry", "Manning"));
+
+		template.requestBody("direct:xah", employees, String.class);
+		mock.assertIsSatisfied();
+	}
 
 }
